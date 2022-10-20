60cdbd288bf8f64b451672f4c367be09c2f7155b
leowipin <leonardopincayjaramillo@gmail.com>
2022-10-20 18:08:42 -0500

    fixes in main class with pmd

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/main.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/main.java
@@ -8,20 +8,9 @@ public class main {
 		if(valor && c.isPermiso()) {
 			c.setPrima(500);
 			CarInsurance.calcularPrima(c);
-			System.out.println(c.getPrima());
 		}else {
 			c.setPrima(-1);
-			if(!c.isPermiso()) {
-				System.out.println("No puede tener seguro porque el cliente no tiene permiso de conducir ");
-
-			}
-			if(!valor) {
-				System.out.println("No puede tener seguro porque el cliente tiene "+c.getEdad()+" años");
-			}
-
 		} 
-		
-		
 	}
 
 }
