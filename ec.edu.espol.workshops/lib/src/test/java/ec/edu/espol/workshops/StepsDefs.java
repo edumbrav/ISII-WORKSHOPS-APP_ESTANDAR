8741154f8dd0582f76b7140372d10df455efdfb9
leowipin <leonardopincayjaramillo@gmail.com>
2022-11-04 23:52:07 -0500

    final commit

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
@@ -1,5 +1,31 @@
 package ec.edu.espol.workshops;
 
+import io.cucumber.java.en.Given; 
+import io.cucumber.java.en.Then; 
+import io.cucumber.java.en.When;
+import static org.junit.Assert.*; 
 public class StepsDefs {
+	
+private char sexo;
+private int prima;
 
+@Given("the customer is a male") 
+public void customer_is_male() {
+	sexo = 'M';
+}
+
+@Given("The customer is {string}")
+public void today_is(char sexo) { 
+	this.sexo = sexo;
+}
+
+@When("Calculate the premium") 
+public void calculate_the_premium() {
+	prima = 2000;
+}
+
+@Then("The customer get a car insurance {int}")
+public void i_should_be_told(String expectedAnswer) { 
+	assertEquals(2000, prima);
+}
 }
